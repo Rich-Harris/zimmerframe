fde60cf56298f1f1f614e9fea269381f7b49fdd7
Rich Harris <rich.harris@vercel.com>
2023-08-20 19:48:35 -0400

    simplify context type arguments

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,17 +6,17 @@ type SpecialisedVisitors<T extends BaseNode, U> = {
 	[K in T['type']]?: Visitor<NodeOf<K, T>, U, T>;
 };
 
-export type Visitor<T, U, V> = (node: T, context: Context<T, U, V>) => V | void;
+export type Visitor<T, U, V> = (node: T, context: Context<V, U>) => V | void;
 
 export type Visitors<T extends BaseNode, U> = T['type'] extends '_'
 	? never
 	: SpecialisedVisitors<T, U> & { _?: Visitor<T, U, T> };
 
-export interface Context<T, U, V> {
-	path: V[];
+export interface Context<T, U> {
+	path: T[];
 	state: U;
 	next: (state: U) => void;
 	skip: () => void;
 	stop: () => void;
-	transform: (node: V, state?: U) => V;
+	transform: (node: T, state?: U) => T;
 }
